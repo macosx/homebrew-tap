aaa7bfe3c95205a3093473cb89fe0029d6e29053
johnsonlee <g.johnsonlee@gmail.com>
2019-03-14 14:54:14 +0800

    Update flutter formula

--- flutter.rb
+++ flutter.rb
@@ -1,9 +1,11 @@
 class Flutter < Formula
-  desc "The Flutter SDK"
+  desc "Build beautiful native apps on iOS and Android from a single codebase"
   homepage "https://flutter.io"
   url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v1.2.1-stable.zip"
   version "1.2.1"
   sha256 "74ac8397ea29720f116980ea00cf60c34430be1f64489b407f7cf95553babbef"
+
+  bottle :unneeded
 
   def install
     system "./bin/flutter"
